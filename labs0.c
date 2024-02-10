80f642ccc4555ca1f03da379ea2e919b483497e6
kyorence <159604947+kyorence@users.noreply.github.com>
2024-02-10 16:40:20 +1000

    Update labs0.c

--- labs0.c
+++ labs0.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
-#include <math.h>
+#include<math.h>
 int main()
 {
-    printf("Hello World");
-
-    return 0;
+    double a,b,c,d,x1,x2;
+    do {
+    printf("Vvedite a:");
+    scanf("%lf",&a);
+    printf("Vvedite b:");
+    scanf("%lf",&b);
+    printf("Vvedite c:");
+    scanf("%lf",&c);
+    d=b*b-4*a*c;
+        printf("d=%lf",d);
+    if (d>0) {
+    x1=(-b+sqrt(d))/(2*a);
+    x2=(-b-sqrt(d))/(2*a);
+    printf("x1=%lf x2=%lf", x1, x2);
+    }
+if(d==0) {
+    x1=(-b/(2*a));
+    printf("x1=%lf ", x1);
+}
+if (d<0){
+    printf("\n net korney");
 }
