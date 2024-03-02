6c1fe328a7f071ddc856713b242ce35dceb185bc
kyorence <159604947+kyorence@users.noreply.github.com>
2024-03-02 15:43:02 +1000

    Update labs2.c

--- labs2.c
+++ labs2.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    char s[80];
-    printf("Vvedite stroky simvolov: ");
-    fgets(s, 80, stdin);
-    for (int i = 0; s[i]; i++) {
-        if (s[i] == 'a' || s[i] == 'b') {
-            s[i] -= 32;
-        }
-    }
-    printf("Preobrazovanaya stroka: %s", s);
-    return 0;
-}
