0b799e571b45f2e6cbd84d6be1eb925345332fff
kyorence <159604947+kyorence@users.noreply.github.com>
2024-03-20 23:43:46 +1000

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+
 int main() {
-    char s[80];
-    printf("Vvedite stroky simvolov: ");
-    fgets(s, 80, stdin);
-    for (int i = 0; s[i]; i++) {
-        if (s[i] == 'a' || s[i] == 'b') {  s[i] -= 32; }
+    char s[81];
+    
+    printf("Vvedite stroky (max. 80 simvolov): ");
+    scanf("%80s", str);
+    for (int i = 0; s[i]; i= '\0; i++) {
+        if (str[i] == 'a' || str[i] == 'b') {
+            str[i] = str[i] - 32;
+        }
     }
-    printf("Preobrazovanaya stroka: %s", s);
+    printf("Izmenen. stroka: %s\n", str);
+    
     return 0;
 }
