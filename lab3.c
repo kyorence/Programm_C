58dd60761bda0740d5cb09a3264fb053bf214d0d
kyorence <159604947+kyorence@users.noreply.github.com>
2024-03-20 23:45:12 +1000

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    char s[81];
+    char str[81];
     
     printf("Vvedite stroky (max. 80 simvolov): ");
     scanf("%80s", str);
-    for (int i = 0; s[i]; i= '\0; i++) {
+    
+    for (int i = 0; str[i] != '\0; i++) {
         if (str[i] == 'a' || str[i] == 'b') {
             str[i] = str[i] - 32;
         }
