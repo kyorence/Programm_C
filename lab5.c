6fb94edf6cb2bcaeeef8a255f1ebfd67ade56b50
kyorence <159604947+kyorence@users.noreply.github.com>
2024-03-29 23:47:37 +1000

    Update lab5.c

--- lab5.c
+++ lab5.c
@@ -5,6 +5,7 @@ double** perform_operation(double** matrix1, double** matrix2, int n, char opera
 int main() {
     int n;
     char operation;
+    
     printf("Введите размер матриц: ");
     scanf("%d", &n);
 
