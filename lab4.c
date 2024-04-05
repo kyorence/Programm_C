2689e6455e2262a61afdac6f273935f3bdcd6afe
kyorence <159604947+kyorence@users.noreply.github.com>
2024-04-05 16:46:29 +1000

    Update lab4.c

--- lab4.c
+++ lab4.c
@@ -1,27 +1,30 @@
-#include <stdio.h> 
-#include <math.h> 
- 
-double Perimeter(double a, double b, double c) { 
-    return a + b + c; 
-} 
-double Ploshad(double a, double b, double c) { 
-    double semiPerimeter = Perimeter(a, b, c) / 2; 
-    return sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c)); 
-} 
+#include <stdio.h>
+#include <math.h>
 
-int main() { 
+double calculatePerimeter(double a, double b, double c) {
+    return a + b + c;
+}
+
+double calculatePloshad(double a, double b, double c) {
+    double semiPerimeter = calculatePerimeter(a, b, c) / 2;
+    return sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+}
+
+int main() {
     double a, b, c;
-    printf(" Введи длины сторон треугольника: ");
-    scanf("%lf %lf %lf", &a, &b, &c); 
- 
-    if (a + b > c && a + c > b && b + c > a) { 
-        double perimeter = Perimeter(a, b, c); 
-        double ploshad = Ploshad(a, b, c); 
-    
-        printf("Периметр треугольника: %lf\n", perimeter); 
-        printf("Площадь треугольника: %lf\n", ploshad); 
-    } else { 
-        printf("Треугольник с такими сторонами не существует!\n"); 
-    } 
-    return 0; 
+
+    printf("Введите длины сторон треугольника: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    if (a + b > c && a + c > b && b + c > a) {
+        double perimeter = calculatePerimeter(a, b, c);
+        double ploshad = calculatePloshad(a, b, c);
+
+        printf("Периметр треугольника: %lf\n", perimeter);
+        printf("Площадь треугольника: %lf\n", ploshad);
+    } else {
+        printf("Треугольник с такими сторонами не существует!\n");
+    }
+
+    return 0;
 }
