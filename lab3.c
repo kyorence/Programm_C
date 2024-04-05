166b768b2161cf81cb48d9b0d964353ee2125b13
kyorence <159604947+kyorence@users.noreply.github.com>
2024-04-05 16:47:01 +1000

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -2,16 +2,17 @@
 
 int main() {
     char str[81];
-    
-    printf("Vvedite stroky (max. 80 simvolov): ");
+
+    printf("Введите строку (макс. 80 символов): ");
     scanf("%80s", str);
-    
-    for (int i = 0; str[i] != '\0; i++) {
+
+    for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] == 'a' || str[i] == 'b') {
             str[i] = str[i] - 32;
         }
     }
-    printf("Izmenen. stroka: %s\n", str);
-    
+
+    printf("Измененная строка: %s\n", str);
+
     return 0;
 }
